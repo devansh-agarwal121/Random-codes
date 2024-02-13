49e3198fd00fc3d85f9f86660aca759a7b95ab08
devansh-agarwal121 <111650966+devansh-agarwal121@users.noreply.github.com>
2024-02-14 03:50:20 +0530

    Update first_ace.cpp

--- first_ace.cpp
+++ first_ace.cpp
@@ -6,7 +6,11 @@ int main () {
 
 /*
 - Problem: On average, how many cards you need to flip from a deck of 52 cards, to get an ace?
-- The code below is basically the brute force approach to solve the problem.
+
+- Code Solution:
+	The code below is basically the brute force approach to solve the problem, i.e. it solves for the following equation
+ 	E = (4/52 * 1)  +  (48/52 * 4/51 * 2)  +  (48/52 * 47/51 * 4/50 * 3)  +  ...  +  (48/52 * 47/51 * ... * 1/5 * 4/4 * 48)
+  	Note: The code's run time can be optimised by using previously solved frac's in next iteration.
 
 - Probabilistic Solution:
 	The four aces divide the space of 48 leftover cards into five parts each of size 48/5
